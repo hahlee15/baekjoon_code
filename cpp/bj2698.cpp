52c09b7c9b4a0ef4f11cf6fb7d5cb8f4dee93e62
hahlee15 <hyunahlee15@gmail.com>
2018-08-07 17:21:10 +0900

    인접한 비트의 개수

---
+++ cpp/bj2698.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+int dp[101][100][2];
+
+int main(void){
+    int testC = 0;
+    int n = 0, k = 0;
+    scanf("%d", &testC);
+
+    dp[1][0][0] = 1;
+    dp[1][0][1] = 1;
+
+    for(int k = 0; k < 100; k++){
+        for(int n = 2; n <= 100; n++){
+            if (k == 0) {
+                dp[n][k][1] += dp[n - 1][k][0];
+            } else {
+                dp[n][k][1] += dp[n - 1][k][0] + dp[n - 1][k - 1][1];
+            }
+            dp[n][k][0] += dp[n - 1][k][0] + dp[n - 1][k][1];
+        }
+    }
+
+    while(testC--){
+        scanf("%d", &n);
+        scanf("%d", &k);
+        printf("%d\n",dp[n][k][0] + dp[n][k][1]);
+    }
+    return 0;
+}
