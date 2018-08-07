5c9150b12f1d65eaa848e9582ea93eee5355149a
hahlee15 <hyunahlee15@gmail.com>
2018-08-07 16:47:26 +0900

    1로 만들기

---
+++ java/bj1463.java
@@ -0,0 +1,34 @@
+﻿import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Main {
+	static int[] dp;
+	static int min = 0;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String temp = br.readLine();
+		int N = Integer.parseInt(temp);
+		int i = 0;
+
+		dp = new int[N + 1];
+		dp[0] = 0;
+		dp[1] = 0;
+
+		if (N == 0 || N == 1) {
+			System.out.print(dp[N]);
+		} else {
+			for (i = 2; i < N + 1; i++) {
+				dp[i] = dp[i - 1] + 1;
+				if (i % 2 == 0 && dp[i] > dp[i / 2] + 1) {
+					dp[i] = dp[i / 2] + 1;
+				}
+                if (i % 3 == 0 && dp[i] > dp[i / 3] + 1) {
+					dp[i] = dp[i / 3] + 1;
+				}
+			}
+			System.out.print(dp[N]);
+		}
+	}
+}
