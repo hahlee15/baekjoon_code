35e287f2b792663caa3e1df7c677385aa0ad2d09
hahlee15 <hyunahlee15@gmail.com>
2018-08-07 17:07:45 +0900

    등록

---
+++ java/bj7287.java
@@ -0,0 +1,11 @@
+﻿import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        int A = 9;
+        String B = "daisy11111";
+                
+        System.out.println(A);
+        System.out.println(B);
+    }
+}
