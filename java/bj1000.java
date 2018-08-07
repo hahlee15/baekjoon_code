e608a37f96640b1879bee310687eb8149089b2f3
hahlee15 <hyunahlee15@gmail.com>
2018-08-07 16:36:21 +0900

    입출력

---
+++ java/bj1000.java
@@ -0,0 +1,15 @@
+﻿import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String temp = br.readLine();
+        String[] temp2 = temp.split(" ");
+        
+        int A = Integer.parseInt(temp2[0]);
+        int B = Integer.parseInt(temp2[1]);
+        
+        System.out.print(A+B);
+    }
+}
