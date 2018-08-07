922f53cf78ad5778d7468edec03981cca167ba05
hahlee15 <hyunahlee15@gmail.com>
2018-08-07 17:28:01 +0900

    쉬운 계단 수

---
+++ cpp/bj10844.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#define num 1000000000
+
+int main(void){
+    int N;
+    scanf("%d", &N);
+    int dp[N+1][10];
+
+    for(int i = 0; i <= 9; i++){    //한 자리 수일 때 0은 제외
+        dp[1][i] = 1;
+    }
+
+    for(int i = 2; i <= N; i++){    //두 자리 이상일 때
+        for(int j = 0; j < 10; j++){
+            if(j == 0){
+                dp[i][0] = dp[i-1][1] % num;
+            }
+            else if(j == 9){
+                dp[i][9] = dp[i-1][8] % num;
+            }
+            else{
+                dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % num;
+            }
+        }
+    }
+    
+    int count = 0;
+    for(int i = 1; i < 10; i++){
+        count = (count + dp[N][i]) % num;
+    }
+    printf("%d\n", count);
+
+    return 0;
+}
