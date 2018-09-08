800ef468473314cfe8ff92ed212282a194054759
hahlee15 <hyunahlee15@gmail.com>
2018-09-09 02:05:18 +0900

    주작 주 주작

---
+++ cpp/bj12792.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N = 0, temp = 0;
+    scanf("%d", &N);
+    for(int i = 1; i <= N; i++){
+        scanf("%d", &temp);
+        if(temp == i){
+            printf("-1\n");
+            return 0;
+        }
+    }
+    printf("1000000007\n");
+    return 0;
+}
